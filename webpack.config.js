c8bbc980e8121af3b8ed01ef92316e2fa0f663a9
Kevin Fung <kevinmcfung@gmail.com>
2017-02-13 19:39:53 -0500

    standardized file extensions

--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,14 @@ var BUILD_DIR = path.resolve(__dirname, 'client/dist');
 var APP_DIR = path.resolve(__dirname, 'client');
 
 module.exports = {
-  entry: APP_DIR + '/index.jsx',
+  entry: APP_DIR + '/index.js',
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js',
   },
   module: {
     loaders: [
-       { test : /\.jsx?/,
+       { test : /\.js?/,
         include : APP_DIR,
         loader : 'babel-loader'
       }
