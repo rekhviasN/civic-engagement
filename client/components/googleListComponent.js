13f56193a64278ac34248e05d9584ce0b14b7b04
Rishi Shah <shah.rishi05@gmail.com>
2017-02-14 12:42:43 -0500

    Implement GoogleList component

--- client/components/googleListComponent.js
+++ client/components/googleListComponent.js
@@ -3,16 +3,16 @@ import ListView from './ListView';
 
 export default class GoogleList extends Component {
   render() {
-    const GoogleResultsOffices = this.props.GoogleResults.offices || [];
-    const GoogleResultsOfficials = this.props.GoogleResults.officials || [];
+    let GoogleResultsOffices = this.props.GoogleResults.offices || [];
+    let GoogleResultsOfficials = this.props.GoogleResults.officials || [];
 
-    const GoogleResults = GoogleResultsOffices.map((Office, index) => {
-      const key = Office._id || index;
-      const profile = GoogleResultsOfficials[Office.officialIndices[0]];
-      const title = Office.name;
-      const name = profile.name;
-      const party = profile.party;
-      const phoneNum = profile.phones[0];
+    let GoogleResults = GoogleResultsOffices.map((Office, index) => {
+      let key = Office._id || index;
+      let profile = GoogleResultsOfficials[Office.officialIndices[0]];
+      let title = Office.name;
+      let name = profile.name;
+      let party = profile.party;
+      let phoneNum = profile.phones[0];
 
       return (<ListView key={key} title={title} name={name} party={party} phone={phoneNum} />);
     });
