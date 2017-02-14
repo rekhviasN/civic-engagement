dc23cd2d73c9fc671699404194dab8e7f75b574d
Kevin Fung <kevinmcfung@gmail.com>
2017-02-14 16:27:58 -0500

    another merge fix

--- client/actions/locationBarActions.js
+++ client/actions/locationBarActions.js
@@ -3,11 +3,7 @@ import Axios from 'axios';
 const ROOT_URL = 'http://localhost:8000/api';
 
 export default function(location) {
-<<<<<<< HEAD
-  const request = Axios.get(`${ROOT_URL}/representatives/${location}`).then(response => console.log(response));
-=======
   const request = Axios.get(`${ROOT_URL}/representatives/${location}`);
->>>>>>> locBarFix
   return {
     type: 'Location_Search',
     payload: request
