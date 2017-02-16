f0fb0aa0e14ad35cc1a1892ccc3eeafa3eb6aca0
Kevin Fung <kevinmcfung@gmail.com>
2017-02-16 15:18:36 -0500

    fixed CORS issue

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const Google = require('./api/google');
 const nytimes = require('./api/nytimes');
+// const wsj = require('./api/wsj');
+// const reuters = require('./api/reuters');
+// const apnews = require('./api/apnews');
 
 const router = express.Router();
 
@@ -8,6 +11,9 @@ router.get('/representatives/:address', Google.getReps);
 router.get('/voting/:address', Google.getVoterInfo);
 
 router.get('/nytimes/:name', nytimes.getArticles);
+// router.get('/wsj/:name', wsj.getArticles); // only allows top headlines
+// router.get('/reuters/:name', reuters.getArticles);
+// router.get('/apnews/:name', apnews.getArticles);
 
 router.get('/test', (req, res) => {
   console.log('got it');
