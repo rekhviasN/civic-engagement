7a6cfdff50d81b60aaf180fb2e92ba54f3a0243b
Kevin Fung <kevinmcfung@gmail.com>
2017-02-15 19:41:51 -0500

    start nytimes route build out

--- server/routes.js
+++ server/routes.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const Google = require('./api/google.js');
+const nytimes = require('./api/nytimes.js');
 
 const router = express.Router();
 
 router.get('/representatives/:address', Google.getReps);
 router.get('/voting/:address', Google.getVoterInfo);
-router.get('/test', (req, res) => { console.log('got it'); res.send({ data: 'data!' }); });
+
+router.get('/nytimes/', nytimes.articleSearch);
+
+router.get('/test', (req, res) => {
+  console.log('got it');
+  res.send({ data: 'data!' });
+});
 
 
 module.exports = router;
