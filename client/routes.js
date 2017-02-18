c649f3b453c12016812995926e3a116a29610ec8
Nino <rekhviasnino@gmail.com>
2017-02-18 11:46:37 -0500

    add sidebar component and container

--- client/routes.js
+++ client/routes.js
@@ -2,13 +2,21 @@ import React from 'react';
 import { Route, IndexRoute, Link } from 'react-router';
 
 // import ReqContainer from './containers/ReqContainer'
-import Req from './containers/reqContainer';
+import Req from './src/req';
 import Counter from './containers/counter2';
+import LocationBar from './components/locationBar';
+import MenuContainer from './containers/sidebarContainer';
 
 const home = function home() {
   return (<div>
     <h1>welcome to the home ppage!</h1>
+    <Link to="/counter">Counter</Link>
+    <br />
     <Link to="/req">Go to request</Link>
+    <br />
+    <Link to="/location">Rep Lookup</Link>
+    <br />
+    <Link to="/menuAnd">Menu with other stuff </Link>
   </div>);
 };
 
@@ -17,5 +25,7 @@ export default (
     <IndexRoute component={home} />
     <Route path="req" component={Req} />
     <Route path="counter" component={Counter} />
+    <Route path="location" component={LocationBar} />
+    <Route path="menuAnd" component={MenuContainer}/>
   </Route>
 );
