5e5b9c1044925639a88b05a8784b9814b4fa0d08
Rishi Shah <shah.rishi05@gmail.com>
2017-02-23 14:22:53 -0500

    Create List component to display meetup events

---
+++ client/components/MeetupEventsList.js
@@ -0,0 +1,48 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import ListItem from './MeetupEventListItem';
+
+class EventListComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    const { results } = this.props.events;
+    console.log('Event List Line 8   ', this.props);
+    let eventList = [];
+    if (results) {
+      eventList = results.map((event, index) =>
+        (
+          <ListItem
+            key={event.id}
+            event={event}
+            index={index}
+          />
+        )
+      );
+    }
+
+    return (
+      <div className="MeetupEventsList">
+        {eventList}
+      </div>
+    );
+  }
+}
+
+EventListComponent.propTypes = {
+  events: PropTypes.arrayOf(PropTypes.object)
+};
+
+EventListComponent.defaultProps = {
+  events: 'n/a'
+};
+
+function mapStateToProps(state) {
+  return {
+    events: state.Meetup.eventResults
+  };
+}
+
+export default connect(mapStateToProps)(EventListComponent);
