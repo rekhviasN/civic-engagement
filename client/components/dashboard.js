98713842eb1a81d7e986d0a75f5aece351500771
Rishi Shah <shah.rishi05@gmail.com>
2017-02-23 16:02:35 -0500

    Add meetup list to dashboard view

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import Map from '../containers/GoogleMapContainer-v2';
 import Search from './meetupSearchBar';
+import EventList from './MeetupEventsList';
 
 const Dashboard = () =>
   (
     <div className="dashboard">
       <div className="dashboard-header">
-        <h2>{'menu fill in'}</h2>
+        <h2>{'PlaceHolder'}</h2>
       </div>
       <div className="row">
-        <Search className="dashboard-side" />
+        <div className="column dashboard-side">
+          <Search className="meetup-search" />
+          <EventList className="meetup-list" />
+        </div>
         <Map />
       </div>
     </div>
