4f6a4c2179b1062b76880bbe05726c0310f7ee5e
Rishi Shah <shah.rishi05@gmail.com>
2017-02-28 14:51:53 -0500

    Add pre-fixed options for top20 search

---
+++ client/constants/top20SelectorOptions.js
@@ -0,0 +1,25 @@
+export const categories = [
+  ['candidate-loan', 'Candidate Loan', 'Loans'],
+  ['contribution-total', 'Contribution Total', 'Total Raised'],
+  ['debts-owed', 'Debts Owed', 'Debt Owed'],
+  ['disbursements-total', 'Disbursements Total', 'Total Spending'],
+  ['end-cash', 'End Cash', 'Remaining Cash'],
+  ['individual-total', 'Individual Total', 'Individual Donations'],
+  ['pac-total', 'PAC Total', 'Contributions from PACs'],
+  // ['receipts-total', 'Receipts Total'],
+  ['refund-total', 'Refund Total']
+];
+
+export const cycles = [
+  1996,
+  1998,
+  2000,
+  2002,
+  2004,
+  2006,
+  2008,
+  2010,
+  2012,
+  2014,
+  2016
+];
