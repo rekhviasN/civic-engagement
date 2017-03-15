8f4f146607f01cb6b0f179ceaeec7e0480698677
Nino <rekhviasnino@gmail.com>
2017-03-15 19:22:32 -0400

    changed root_url of reps api request

--- client/actions/locationBarActions.js
+++ client/actions/locationBarActions.js
@@ -1,6 +1,6 @@
 import Axios from 'axios';
 
-const ROOT_URL = 'http://localhost:8000/api';
+const ROOT_URL = 'https://civicsportal.herokuapp.com/api';
 
 export default function (location) {
   const request = Axios.get(`${ROOT_URL}/representatives/${location}`);
